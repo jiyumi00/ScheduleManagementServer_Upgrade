51897aed7bd4fe7373daceb20a5a2b4913a4610a
Jiyun <jiyumi00@naver.com>
2024-08-26 18:41:55 +0900

    프로젝트 생성

---
+++ src/main/java/com/sparta/ScheduleManagement_Upgrade/ScheduleManagementUpgradeApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.ScheduleManagement_Upgrade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduleManagementUpgradeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScheduleManagementUpgradeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sparta/ScheduleManagement_Upgrade/ScheduleManagementUpgradeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.ScheduleManagement_Upgrade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleManagementUpgradeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
